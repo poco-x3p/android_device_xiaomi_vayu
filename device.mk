b7c511ad59299feacf62d94a8a7395fa6da02062
raghavt20 <raghavtandon2000@gmail.com>
2021-10-04 18:17:06 +0530

    vayu: Rename fingerprint services and copy hardware permissions

--- device.mk
+++ device.mk
@@ -99,6 +99,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.vayu
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
