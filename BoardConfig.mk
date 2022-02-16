fc6c8090f22674f516881ebe5340468a71ba0527
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-02-16 16:07:54 +0530

    vayu: power: Adapt xiaomi-touch DT2W extension to AOSPA power HAL
    - https://github.com/pa-gr/android_vendor_aospa/commit/027c4d9fea9bce9df0b8de6314e03934d97f8652
    - Also add required AOSPA overlays for Double Tap Gesture
    
    Change-Id: I797a2840df9cf9befc5b1cc7e23282d08ea10952
    Signed-off-by: raghavt20 <raghavt20@aospa.co>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -136,7 +136,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 BOARD_USES_METADATA_PARTITION := true
 
 # Power
-TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+TARGET_POWER_FEATURE_EXT_LIB := //$(DEVICE_PATH):libpowerfeature_ext_vayu
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
